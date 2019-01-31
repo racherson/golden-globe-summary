198ce37d89a361a9fdd9ed9d3d6c77b69401a09a
Rachel Anderson <rachelanderson2020@u.northwestern.edu>
2019-01-31 23:05:38 +0000

    starting code

--- gg_api.py
+++ gg_api.py
@@ -1,4 +1,12 @@
+import nltk
+
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+hosts = []
+awards = []
+nominees = []
+winners = []
+presenters = []
+
 
 def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
@@ -6,11 +14,13 @@ def get_hosts(year):
     # Your code here
     return hosts
 
+
 def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
     # Your code here
     return awards
+
 
 def get_nominees(year):
     '''Nominees is a dictionary with the hard coded award
@@ -19,12 +29,14 @@ def get_nominees(year):
     # Your code here
     return nominees
 
+
 def get_winner(year):
     '''Winners is a dictionary with the hard coded award
     names as keys, and each entry containing a single string.
     Do NOT change the name of this function or what it returns.'''
     # Your code here
     return winners
+
 
 def get_presenters(year):
     '''Presenters is a dictionary with the hard coded award
@@ -33,14 +45,16 @@ def get_presenters(year):
     # Your code here
     return presenters
 
+
 def pre_ceremony():
     '''This function loads/fetches/processes any data your program
     will use, and stores that data in your DB or in a json, csv, or
     plain text file. It is the first thing the TA will run when grading.
     Do NOT change the name of this function or what it returns.'''
     # Your code here
-    print "Pre-ceremony processing complete."
+    print("Pre-ceremony processing complete.")
     return
+
 
 def main():
     '''This function calls your program. Typing "python gg_api.py"
@@ -50,6 +64,7 @@ def main():
     what it returns.'''
     # Your code here
     return
+
 
 if __name__ == '__main__':
     main()
