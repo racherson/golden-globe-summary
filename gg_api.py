aed3968d0d3d2817424c8b1c31344c59a87d5090
Hunter Kern <Hunterkern2015@u.northwestern.edu>
2019-02-07 21:06:24 -0600

    Add files via upload
    Consolidated Code

--- gg_api.py
+++ gg_api.py
@@ -1,20 +1,29 @@
 import sys
 import re
 import string
-import nltk
-nltk.download('stopwords')
 import time
 import json
 import copy
 from pprint import pprint
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+nernlp = spacy.load('en_core_web_sm', disable=['parser', 'tagger'])
+custom_stop_words = [
+        'Golden Globes', 'goldenglobes', '@', 'golden globes', 'RT', 'GoldenGlobes', '\n', '#', "#GoldenGlobes"
+    ]
+from spacy.tokenizer import Tokenizer
+tokenizer = Tokenizer(nlp.vocab)
 
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 
-hosts = {}
-awards = {}
+tweets = {}
+hosts = []
+awards = []
 nominees = {}
 winners = {}
 presenters = {}
+
 
 def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
@@ -60,8 +69,38 @@ def pre_ceremony():
     plain text file. It is the first thing the TA will run when grading.
     Do NOT change the name of this function or what it returns.'''
     # Your code here
+    print('Starting...')
+    global tweets
+    # names = {}
+    # docs = []
+    start_time = time.time()
+    tweets_2013 = load_data('gg2013.json')
+    pprint(tweets_2013[1])
+    tweets[2013]=tweets_2013
+    tweets_2013 = extract_text(tweets_2013)
+    with open('processed_gg2013.json', 'w') as f:
+        json.dump(tweets_2013, f)
+    # gen, ner = parse_text(tweets_2013)
+    # for doc in ner:
+    #     for ent in doc.ents:
+    #         if (ent.text not in custom_stop_words) and (ent.text not in names) and 'http' not in ent.text and 'RT' not in ent.text and '@' not in ent.text and '#' not in ent.text:
+    #             names[ent.text] = 1
+    #         elif ent.text in names:
+    #             names[ent.text] += 1
+    # for doc in gen:
+    #     # print(doc)
+    #     docs.append(doc)
+    #     # pass
+    # for k, v in list(names.items()):
+    #     if v <= 50:
+    #         del names[k]
+    # print(docs[1])
+    # print(names)
+    end_time = time.time()
+    print('Time', str(end_time - start_time))
     print("Pre-ceremony processing complete.")
     return
+
 
 # load json file as dictionary
 # file_name: 'gg2013.json'
@@ -71,47 +110,64 @@ def load_data(file_name):
         tweets = json.load(f)
     return tweets
 
-# remove nltk English stop words from all tweets
-def remove_stop_words(tweets):
-    stops = set(nltk.corpus.stopwords.words("english"))
+
+def token_filter(token):
+    return not (token.is_punct | token.is_space | token.is_stop | len(token.text) <= 3 | '@' not in token.text)
+
+
+def extract_text(tweets):
+    tweet_text = []
     filtered_tweets = copy.deepcopy(tweets)
     for tweet in filtered_tweets:
-        words = tweet['text'].split() # array of word
-        lower_words = [word.lower() for word in words]
-        tweet['text'] = ' '.join([word for word in lower_words if word not in stops])
+         tweet_text.append(tweet['text'])
+    return tweet_text
 
-    return filtered_tweets
+
+def parse_text(tweets):
+    spacy_tweets = []
+    spacy_tweets = tokenizer.pipe(tweets)
+    ner = nernlp.pipe(tweets)
+    return spacy_tweets, ner
+
 
 def main():
+    for w in custom_stop_words:
+        nlp.vocab[w].is_stop = True
     '''This function calls your program. Typing "python gg_api.py"
     will run this function. Or, in the interpreter, import gg_api
     and then run gg_api.main(). This is the second thing the TA will
     run when grading. Do NOT change the name of this function or
     what it returns.'''
-    # Your code here
-    print ('Starting...')
-    start_time = time.time()
-
-    # save filtered tweets 2013 as 'filtered_2013.json'
-    tweets_2013 = load_data('gg2013.json')
-    pprint (tweets_2013[1])
-    filtered_tweets_2013 = remove_stop_words(tweets_2013)
-    with open('filtered_2013.json', 'w') as fp:
-        json.dump(filtered_tweets_2013, fp)
-    pprint (filtered_tweets_2013[1])
-
-    # save filtered tweets 2015 as 'filtered_2015.json'
-    tweets_2015 = load_data('gg2015.json')
-    pprint (tweets_2015[1])
-    filtered_tweets_2015 = remove_stop_words(tweets_2015)
-    with open('filtered_2015.json', 'w') as fp:
-        json.dump(filtered_tweets_2015, fp)
-    pprint (filtered_tweets_2015[1])
-
-    end_time = time.time()
-    print ('Time', str(end_time - start_time))
-    print ('Finished!')
+    pre_ceremony()
+    tweets, ner = parse_text(tweets[2013])
+    n = 100000 # limit number of tweets we search
+    for tweet in tweets:
+        tokens = []
+        next_flag = False
+        should_flag = False
+        for token in tweet:
+            t = token.text.lower()
+            if t == 'next': # ignore tweets about next year
+              	next_flag = True
+            if t == 'should': # ignore opinion tweets
+                should_flag = True
+                continue
+            if not token_filter(token):
+                continue
+            tokens.append(token.lemma_.lower())
+        for token in tokens:
+            if token == 'host' and not (next_flag and should_flag):
+                for ent in tweet.ents:
+                    if token_filter(ent, False):
+                        hosts.append(ent.text.lower())
+                pprint(tweet)
+                break # maybe remove?
+            # awards checking, maybe use hunter's names collection to try and do awards idk
+        if n == 0:
+            break
+        n -= 1
     return
+
 
 if __name__ == '__main__':
     main()
